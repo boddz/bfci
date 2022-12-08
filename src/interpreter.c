b9893c890bd1cd0d221f30be85f912d85f0658d9
Salad <salad@pop-os.localdomain>
2022-12-08 17:29:16 +0000

    Added Bf to C translator
    Added a function in interpreter.h/c which will take Brainf*** code and translate it into C code and write said C code to a file of choice.

--- src/interpreter.c
+++ src/interpreter.c
@@ -12,17 +12,19 @@
 #include "interpreter.h"
 
 
+/* Returns true if the specified file is of a valid extention type.
+ * Takes one argument of char*. */
 bool check_file_ext(char* file_name) {
   int before_ext = 0;
 
-  while(*file_name != '.') { /* Push array pointer to start of ext (.bf). */
+  while(*file_name != '.') { /* Move array pointer to fullstop/ ext offset. */
     ++before_ext;
     ++file_name;
   }
 
-  if(before_ext == 0) return false; /* No proceeding chars, avoid seg fault. */
+  if(before_ext == 0) return false; /* No chars before '.' avoid seg fault. */
 
-  /* Return true if pointed to section of array matches valid bf ext. */
+  /* Allowed Brainf*** extention types (I think this is all). */
   if(!strcmp(file_name, ".b") || !strcmp(file_name, ".bf")) return true;
   if(!strcmp(file_name, ".B") || !strcmp(file_name, ".BF")) return true;
   if(!strcmp(file_name, ".bF") || !strcmp(file_name, ".Bf")) return true;
@@ -31,23 +33,31 @@ bool check_file_ext(char* file_name) {
 }
 
 
-char* bf_file_to_array(FILE* input_file)
+/* Returns a pointer to a calloced array containinng the contents of file.
+ * Takes one argument of type FILE* and closes handle before return.
+ * This function assumes that the caller will free the array manually. */
+ char* bf_file_to_array(FILE* input_file)
 {
-  /* Get file size and rewind pointer for later use. */
+  /* Get file size and rewind cursor/pointer for later use. */
   fseek(input_file, 0, SEEK_END);
   unsigned int file_size = ftell(input_file);
   rewind(input_file);
 
+  /* Calloc and dump contents into the array, return the pointer. */
   char* out_array = calloc(file_size + 1, sizeof(char));
-
-  /* Dump the contents of file into the calloced array and append null term. */
   for(unsigned int i = 0; i < file_size; i++) out_array[i] = fgetc(input_file);
   out_array[file_size + 1] = '\0';
+
+  fclose(input_file);
 
   return out_array;
 }
 
 
+/* Returns an int execution/ error code to indicate status of execution ~
+ * == 0 : the execution of interpreter was successful, no errors.
+ * != 0 : encountered an error during execution process.
+ * Takes one argument of type char*. */
 int bf_interpreter(char* bf_code)
 {
   unsigned int loop;
@@ -109,5 +119,50 @@ int bf_interpreter(char* bf_code)
     }
     bf_code++;
   }
+  return 0;
+}
+
+
+/* Returns an int execution/ error code to indicate status of translation.
+ * == 0 : translation Brainf*** to C translated succesfully.
+ * != 0 : encountered some kind of error during the translation process.
+ * Takes two arguments of type char* . */
+int bf_into_c(char* bf_code, char* output)
+{
+  /* Ensure the file name for the output file is actually specified. */
+  if(!*output) {
+    fprintf(stderr, "aborted: a name for the output C file is required\n");
+    return 1;
+  }
+
+  FILE* c_file_out = fopen(output, "w");
+
+  fprintf(c_file_out,
+	  "#include <stdio.h>\n"
+	  "char tape[%i] = {0};\n"
+	  "char* p = tape;\n"
+	  "int main(void)\n{\n",
+	  TAPESIZE);
+
+  while(*bf_code) {
+    switch(*bf_code) {
+    case '>': fprintf(c_file_out, "++p;\n"); break;
+    case '<': fprintf(c_file_out, "--p;\n"); break;
+    case '+': fprintf(c_file_out, "++*p;\n"); break;
+    case '-': fprintf(c_file_out, "--*p;\n"); break;
+    case '.': fprintf(c_file_out, "putchar(*p);\n"); break;
+    case ',':
+      fprintf(c_file_out, "*p = getchar();\nif(*p == '\n') *p = getchar();\n");
+      break;
+    case '[': fprintf(c_file_out, "while(*p) {\n"); break;
+    case ']': fprintf(c_file_out, "}\n"); break;
+    }
+    bf_code++;
+  }
+
+  fprintf(c_file_out, "return 0;\n}\n");
+
+  fclose(c_file_out);
+
   return 0;
 }
--- src/interpreter.h
+++ src/interpreter.h
@@ -10,6 +10,7 @@
 bool check_file_ext(char*); /* Not valid ext: b, B, bf, BF. Error & exit. */
 char* bf_file_to_array(FILE*); /* Bf File -> calloce array caller must free. */
 int bf_interpreter(char*); /* Bf code interpreter. */
+int bf_into_c(char*, char*); /* Translate Bf code into C & write to file. */
 
 
 #endif
